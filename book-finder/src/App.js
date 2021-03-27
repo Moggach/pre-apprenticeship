1e7526a0fdd02e6f7460d81222c84b0fb420f0a7
“Moggach” <acunnane13@gmail.com>
2021-03-27 16:14:15 +0000

    refactor css

--- book-finder/src/App.js
+++ book-finder/src/App.js
@@ -1,80 +1,56 @@
-import React, { Component } from 'react';
-import './App.css';
-import axios from 'axios';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
+import Footer from "./components/Layout/footer.js";
+import Navbar from "./components/Layout/navbar.js";
+import Search from "./components/Layout/Search.js";
+import Description from "./components/Description.js";
 
-import Layout from './components/Layout/layout.js'
-import Search from './components/Layout/Search.js'
-import Description from './components/Description.js'
-
-
-class App extends Component  {
-
+class App extends Component {
   state = {
-    books: []
-}
+    books: [],
+  };
 
- 
- searchBooks = async text => {
-   const res = await axios.get(
-   `https://www.googleapis.com/books/v1/volumes?q=${text}`)
-   this.setState({books: res.data.items})
-
-  
- };
- 
-
+  searchBooks = async (text) => {
+    const res = await axios.get(
+      `https://www.googleapis.com/books/v1/volumes?q=${text}`
+    );
+    this.setState({ books: res.data.items });
+  };
 
   render() {
     return (
+      <div>
+        <Navbar />
 
-<Layout>
-<div>
-<Search searchBooks={this.searchBooks}/></div>
+        <Search searchBooks={this.searchBooks} />
 
+        <div className="container">
+          {this.state.books.map(
+            ({ volumeInfo: { title, description, imageLinks } }) => (
+              <div className="cards">
+                <img className="img-box" src={imageLinks.thumbnail}></img>
 
-        <div style={bookStyle}>
-        {
-          this.state.books.map(({ volumeInfo: { title, description, imageLinks  } }) => (
-            <div style={{ boxShadow: "0px 3px 15px rgba(0,0,0,0.2)", backgroundColor: "#ffffff", borderRadius: "0.5rem", height: "800px", overflow: "hidden"}}>
-            
-              <img style={{width: "50%", display: "block", margin: "auto", marginTop: "1.5rem"}} src={imageLinks.thumbnail}></img>
-              
-          
                 <div className="title">{title}</div>
-              
-          
-                
-                <Description ReadMore = {description}/>
+
+                <Description ReadMore={description} />
                 <div>
-                <div className="tags-container">
-                <div className="tags">#Lorem Impsum</div>
-                <div className="tags">#Lorem Impsum</div>
-                <div className="tags">#Lorem Impsum</div>
+                  <div className="tags-container">
+                    <div className="tags">#Lorem Impsum</div>
+                    <div className="tags">#Lorem Impsum</div>
+                    <div className="tags">#Lorem Impsum</div>
+                  </div>
                 </div>
-                </div>
-                </div>
-  
-              
-         
-          
-          ))
-        }
+              </div>
+            )
+          )}
+        
+        </div>
+        <Footer />
       </div>
-    
-      </Layout>
     );
-    
-      };
-  
-    };
-const bookStyle = {
-  display: 'grid',
-  gridTemplateColumns: 'repeat(3, 1fr)',
-  gridGap: '1rem',
- 
-
-};
-
+  }
+}
 
 
 
--- book-finder/src/components/Layout/navbar.js
+++ book-finder/src/components/Layout/navbar.js
@@ -1,17 +1,24 @@
-import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBookOpen} from '@fortawesome/free-solid-svg-icons'
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookOpen } from "@fortawesome/free-solid-svg-icons";
 
 export class navbar extends Component {
-    render() {
-        return (
-        <div className="navbar" style={{ color: "#8A2BE2", display: "flex", justifyContent: "center", marginTop: '1rem'}}>
-        <FontAwesomeIcon className="icons" icon={faBookOpen} size = '4x' color ='#8A2BE2'/>
-        <h1 style={{marginLeft: "0.5rem", marginTop: "0.3rem"}}>BookFinder</h1>
-        
+  render() {
+    return (
+      <div
+        className="navbar">
+        <FontAwesomeIcon
+          className="icons"
+          icon={faBookOpen}
+          size="4x"
+          color="#8A2BE2"
+        />
+        <div className="logo-text">
+          BookFinder
         </div>
-        )
-    }
+      </div>
+    );
+  }
 }
 
 export default navbar;
