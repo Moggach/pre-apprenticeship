ab6294e79f122438e27e719fe4e0cd682db31222
“Moggach” <acunnane13@gmail.com>
2021-04-07 23:05:03 +0100

    replaced read more with elipsis. styled buy link

--- book-finder/src/App.js
+++ book-finder/src/App.js
@@ -6,8 +6,6 @@ import Navbar from "./components/Layout/navbar.js";
 import Search from "./components/Layout/Search.js";
 import Description from "./components/Description.js";
 
-
-
 class App extends Component {
   state = {
     books: [],
@@ -18,61 +16,48 @@ class App extends Component {
       `https://www.googleapis.com/books/v1/volumes?q=${text}`
     );
     this.setState({ books: res.data.items });
-
   };
 
- 
-
-    
   render() {
     return (
-     
       <div>
         <Navbar />
 
         <Search searchBooks={this.searchBooks} />
 
         <div className="container">
-          {this.state.books.map( book => (
-              <div className="cards">
-                <img className="img-box" src={book.volumeInfo.imageLinks.thumbnail}></img>
+          {this.state.books.map((book) => (
+            <div className="cards">
+              <img
+                className="img-box"
+                src={book.volumeInfo.imageLinks.thumbnail}
+              ></img>
 
-                <div className="title">{book.volumeInfo.title}</div>
+              <div className="title">{book.volumeInfo.title}</div>
 
-                <Description ReadMore={book.volumeInfo.description} />
-           
-
-          
-                <div className="link-to-buy"> <a href={`${book.saleInfo.buyLink}`}target="_blank">Link to Buy</a></div>
-               
-                
-                <div>
-                  <div className="tags-container">
-                    <div className="tags">#Lorem Impsum</div>
-                    <div className="tags">#Lorem Impsum</div>
-                    <div className="tags">#Lorem Impsum</div>
-                  </div>
-                </div>
+              <div className="description">
+                {" "}
+                <Description
+                  ReadMore={book.volumeInfo.description}
+                ></Description>
               </div>
-            )
-          )
-            }
 
-        
+              <div className="link-to-buy">
+                {" "}
+                <a href={`${book.saleInfo.buyLink}`} target="_blank">
+                  Link to Buy
+                </a>
+              </div>
+
+           
+            </div>
+          ))}
         </div>
-      
-       
+
         <Footer />
       </div>
-    
     );
   }
 }
 
-
-
 export default App;
-
-
-
-
